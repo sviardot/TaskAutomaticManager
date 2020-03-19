14f5ad58ecc7162da7e6d6a1b4cd1c3aff9194aa
serge viardot <serge.viardot@wanadoo.fr>
2020-03-19 22:11:21 +0100

    log

--- main.cpp
+++ main.cpp
@@ -1,17 +1,41 @@
 #include <iostream>
+#include <signal.h>
+#include <sstream>
+#include <string>
+#include <cstring>
 
 #include "csystemargument.h"
 #include "DateUtility.h"
 #include "debug.h"
 
+
 using namespace std;
 using namespace Utility;
+
+void signalHandler( int signum ) {
+    cout << "Interrupt signal (" << signum << ") received.\n";
+    /*stringstream sstr;
+    string str;
+
+    sstr << "Interrupt signal (" << signum << ") received.\n";
+    sstr >> str;
+    char * cstr = new char [str.length()+1];
+    std::strcpy (cstr, str.c_str());
+    log_err(cstr);*/
+    log_err("signal caught");
+   // cleanup and close up stuff here
+   // terminate program
+
+   exit(signum);
+}
 
 int main()
 {
     DateUtility dateUtility;
 
     log_err("test");
+    // register signal SIGINT and signal handler
+    signal(SIGINT, signalHandler);
 
     cout << dateUtility.Now() << endl;
     cout << dateUtility.LogNow() << endl;
